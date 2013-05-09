23ab7bfe981729a7eaf26f9551dc6651709925f3
ghajba <gabor.hajba@gmail.com>
2013-05-09 21:15:36 +0200

    added XSDValidator for Python
    first version of the Python implementation of the XSD Validator

---
+++ XSD/README.txt
@@ -0,0 +1,7 @@
+This is a simple XML against XSD Validator for Python 2.7 - 3.2
+Eventually it could work with other versions too.
+
+To run the Script you have to install lxml (http://lxml.de)
+
+Usage:
+python XSDValidator.py <list of XML files> <list of XSD files>
---
+++ XSD/XSDValidator.py
@@ -0,0 +1,45 @@
+# Simple XML against XSD Validator for Python 2.7 - 3.2
+# to run this script you need additionally: lxml (http://lxml.de)
+# author: Gabor Laszlo Hajba, 2013
+
+import sys
+from lxml import etree
+
+xsd_files = []
+xml_files = []
+   
+def usage():
+    print("Usage: ")
+    print("python XSDValidator.py <list of xml files> <list of xsd files>")
+    print("At least one .xml and one .xsd file is required.")
+    
+def validate_files():
+    """ validates every xml file against every schema file"""
+    for schema in xsd_files:
+        xmlschema = etree.XMLSchema(file=schema)
+        for file in xml_files:
+            xml_file = etree.parse(file)
+            if xmlschema.validate(xml_file):
+                print(file + " is valid against " + schema)
+            else:
+                log = xmlschema.error_log
+                print(file + " is not valid against " + schema)
+                for error in iter(log):
+                    print("\tReason: " + error.message)
+    
+def main():
+    if(len(sys.argv) < 3):
+        usage()
+        exit()
+    for arg in sys.argv[1:]:
+        if arg.endswith(".xml"):
+            xml_files.append(arg)
+        elif arg.endswith(".xsd"):
+            xsd_files.append(arg)
+    if len(xsd_files) < 1 or len(xml_files) < 1:
+        usage()
+        exit()
+    validate_files()
+        
+if __name__ == '__main__':
+    main()
