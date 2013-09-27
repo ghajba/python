42d2a5836ac8b00a4d396d904a91f1a07aba94f7
Gábor László Hajba <gabor.hajba@gmail.com>
2013-09-27 11:37:58 +0200

    cleanup
    a bit code cleanup and adding InvalidInputError to indicate wrong files

--- Simplex/Simplex.py
+++ Simplex/Simplex.py
@@ -11,6 +11,15 @@
 
 import sys
 
+
+class InvalidInputError(Exception):
+    def __init__(self, expected, got):
+        self.expected = expected
+        self.got = got
+
+    def __str__(self):
+        return "Invalid input length. Expected {0} lines but got {1}".format(self.expected, self.got)
+
 # global variables
 #m = 3
 #n = 4
@@ -31,43 +40,49 @@ z = []
 
 def init(file_location):
     """ This method initializes the variables with the data from the file """
-    global n,m,b,z,basic,non_basic,A_matrix
+    global n, m, b, z, basic, non_basic, A_matrix
     input_data_file = open(file_location, 'r')
     input_data = ''.join(input_data_file.readlines())
     input_data_file.close()
 
     lines = input_data.split('\n')
+    if len(lines) < 6:
+        raise InvalidInputError(6, len(lines))
 
     m = int((lines[0].split())[0])
     n = int((lines[0].split())[1])
-    basic = map(int,lines[1].split())
-    non_basic = map(int,lines[2].split())
-    b = map(float,lines[3].split())
+    basic = map(int, lines[1].split())
+    non_basic = map(int, lines[2].split())
+    b = map(float, lines[3].split())
     A_matrix = []
-    for i in range(4,m+4):
-        A_matrix.append(map(float,lines[i].split()))
-    z = map(float,lines[m+4].split())
-    
+    for i in range(4, m+4):
+        A_matrix.append(map(float, lines[i].split()))
+    z = map(float, lines[m+4].split())
+
+
 def final_dictionary():
     """ Based on the objective line this method looks up if the dictionary is final or not. """
     return sum(1 for zi in z[1:] if zi > 0) == 0
 
+
 def find_entering():
-    """ This method looks up the entering variable using Bland's Rule: selects the variable with the least index if more possible entering variables exist. """
+    """ This method looks up the entering variable using Bland's Rule: selects the variable
+        with the least index if more possible entering variables exist. """
     actual = None
-    for i in range(1,len(z)):
-        if z[i] > 0 and (non_basic[i-1] < actual or actual == None):
+    for i in range(1, len(z)):
+        if z[i] > 0 and (non_basic[i-1] < actual or actual is None):
             actual = non_basic[i-1]
     return actual
+
 
 def find_leaving(e_idx):
     """ Based on the index of the entering variable (e_idx) this method looks up the leaving variable
         using Bland's Rule: selects the variable with the least index if more possible leaving variables exist. """
     actual = None
     bound = None
-    for i in range(0,len(A_matrix)):
-        if A_matrix[i][e_idx] < 0 and (-1*b[i]/A_matrix[i][e_idx] <= bound or bound == None):
-            if actual == None:
+    for i in range(0, len(A_matrix)):
+        if A_matrix[i][e_idx] < 0 and (-1*b[i]/A_matrix[i][e_idx] <= bound or bound is None):
+            if actual is None:
                 actual = basic[i]
             elif -1*b[i]/A_matrix[i][e_idx] == bound and actual > basic[i]:
                 actual = basic[i]
@@ -76,17 +91,19 @@ def find_leaving(e_idx):
             bound = -1*b[i]/A_matrix[i][e_idx]
     return actual
 
-def get_objective_value(e_idx,l_idx):
+
+def get_objective_value(e_idx, l_idx):
     """ Based on the index of the entering variable (e_idx) and on the index of the leaving variable (l_idx)
         this method calculates the objective value of the dictionary """
     return z[0]+((b[l_idx]/(-1*A_matrix[l_idx][e_idx]))*z[e_idx+1])
 
-def recalculate_dictionary(e_idx,l_idx):
+
+def recalculate_dictionary(e_idx, l_idx):
     """ Based on the index of the entering variable (e_idx) and on the index of the leaving variable (l_idx)
         this method rearranges the dictionary, making the entering variable enter, the leaving variable leave """
     global basic, non_basic, z, b, A_matrix
     
-    obj_val = get_objective_value(e_idx,l_idx)
+    obj_val = get_objective_value(e_idx, l_idx)
     
     # 0. secure the value of the entering variable and the objective value
     # 1. swap places: entering and leaving
@@ -103,11 +120,11 @@ def recalculate_dictionary(e_idx,l_idx):
     non_basic[e_idx] = basic[l_idx]
     basic[l_idx] = entering
 
-    A_matrix[l_idx][e_idx] = -1.0 # the leaving variable has to be -1.0 as entering
+    A_matrix[l_idx][e_idx] = -1.0  # the leaving variable has to be -1.0 as entering
     
     # 2.
     A_matrix[l_idx] = [i/entering_val for i in A_matrix[l_idx]]
-    b[l_idx] = b[l_idx]/entering_val
+    b[l_idx] /= entering_val
 
     # 3.
     for i in range(len(basic)):
@@ -123,11 +140,12 @@ def recalculate_dictionary(e_idx,l_idx):
     # 5.
     z[0] = obj_val
     coeff = z[e_idx+1]
-    for i in range(1,len(z)):
+    for i in range(1, len(z)):
         if i != (e_idx+1):
             z[i] += (coeff*A_matrix[l_idx][i-1])
         else:
             z[i] = (coeff*A_matrix[l_idx][i-1])
+
 
 def solve_problem():
     """ This method solves the problem initialized from the input data """
@@ -137,26 +155,27 @@ def solve_problem():
         entering = find_entering()
         e_idx = non_basic.index(entering)
         leaving = find_leaving(e_idx)
-        solution = None
-        if leaving != None:
+        if leaving is not None:
             l_idx = basic.index(leaving)
         else:
             print "UNBOUNDED"
             break
-        recalculate_dictionary(e_idx,l_idx)
+        recalculate_dictionary(e_idx, l_idx)
         pivoting_steps += 1
         if final_dictionary():
             break
     print "Objective Value: {0:.5f} \nPivoting Steps: {1}".format(z[0], pivoting_steps)
-    write_output(z[0],pivoting_steps)
+    write_output(z[0], pivoting_steps)
+
 
 def print_simplex_dict():
     """ This method prints the simplex dictionary to the console.
         By default it uses global variables defined above to handle as the dictionary.
         However you can use the declaration of the method as above, providing every information as a method parameter.
-        For large dictionaries the console output would become unreadable so it is preferred to redirect the output into a file."""
+        For large dictionaries the console output would become unreadable
+        so it is preferred to redirect the output into a file."""
     for i in range(len(basic)):
-        line_text = "x_{0} = {1:.3f}".format(basic[i],b[i])
+        line_text = "x_{0} = {1:.3f}".format(basic[i], b[i])
         for j in range(len(non_basic)):
             if A_matrix[i][j] < 0:
                 line_text = line_text+" - {0:.3f}x_{1}".format(A_matrix[i][j]*-1, non_basic[j])
@@ -167,13 +186,15 @@ def print_simplex_dict():
     line_text = " z  = {0:.3f}".format(z[0])
     for i in range(len(non_basic)):
         if z[i+1] < 0:
-            line_text = line_text + " - {0:.3f}x_{1}".format(z[i+1]*-1,non_basic[i])
+            line_text = line_text + " - {0:.3f}x_{1}".format(z[i+1]*-1, non_basic[i])
         else:
-            line_text = line_text + " + {0:.3f}x_{1}".format(z[i+1],non_basic[i])
+            line_text = line_text + " + {0:.3f}x_{1}".format(z[i+1], non_basic[i])
     print line_text
 
+
 def write_output(objective_value, pivoting_steps):
-    """ This method writes the output to a file with the name of the input file -- adding a '.sol' at the end of the file name. """
+    """ This method writes the output to a file with the name of the input file --
+        adding a '.sol' at the end of the file name. """
     output = open(filename+".sol", 'w')
     if pivoting_steps < 1:
         output.write("UNBOUNDED")
@@ -188,4 +209,5 @@ if __name__ == '__main__':
         init(filename)
         solve_problem()
     else:
-        print 'This test requires an input file.  Please select one from the data directory. (i.e. python Simplex.py ./data/part1.dict)'
+        print 'This test requires an input file.  Please select one from the data directory. ' \
+              '(i.e. python Simplex.py ./data/part1.dict)'
