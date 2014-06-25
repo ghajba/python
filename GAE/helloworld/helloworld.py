ada94983e0ac2eadf7e217a9c8eab11a5c13b4dc
Gábor László Hajba <gabor.hajba@gmail.com>
2014-06-25 11:21:07 +0200

    added form to the page to make the weather-query more interactive.

--- GAE/helloworld/helloworld.py
+++ GAE/helloworld/helloworld.py
@@ -5,15 +5,54 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 import weather_endpoint
 
+MAIN_PAGE_HTML_HEAD = """\
+<html>
+  <head>
+    <title>Simple GAE Project by GHajba</title>
+  </head>
+  <body>
+  	<h1>Welmoe to this all-in-one GAE project</h1>
+  	<div>This is a simple web application on the GAE created in Python</div>
+  	<h3>Printing "Hello World" in Chinese:</h3>
+"""
+
+MAIN_PAGE_HTML_FOOT = """\
+    <hr />
+  </body>
+</html>
+"""
+
+MAIN_PAGE_HTML_WEATHER_FORM = """\
+    <hr />
+    <form action="/weather" method="get">
+      <div>Look up the weather of a US-City by its ZIP-Code:</div>
+      <div><input name="zip" type="number" size="10" min="10000" max="99999" required></div>
+      <div><input type="submit" value="Ask for the weather"></div>
+    </form>
+  </body>
+</html>
+"""
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(MAIN_PAGE_HTML_HEAD)
         self.response.write(u'Hello World in Chinese is: 你好世界')
-        self.response.write('<hr/>')
-        weather_description, weather_picture = weather_endpoint.get_chicago_weather()
-        self.response.write('<img src="'+weather_picture+'" /> ')
+        self.response.write(MAIN_PAGE_HTML_WEATHER_FORM)
+        self.response.write(MAIN_PAGE_HTML_FOOT)
+
+class Weather(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(MAIN_PAGE_HTML_HEAD)
+        weather_description, weather_picture = weather_endpoint.get_city_weather(self.request.get('zip'))
+        if weather_picture is not None:
+            self.response.write('<img src="'+weather_picture+'" /> ')
         self.response.write(weather_description)
+        self.response.write(MAIN_PAGE_HTML_WEATHER_FORM)
+        self.response.write(MAIN_PAGE_HTML_FOOT)
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/weather', Weather)
 ], debug=True)
--- GAE/helloworld/weather_endpoint.py
+++ GAE/helloworld/weather_endpoint.py
@@ -1,27 +1,16 @@
+# coding=utf-8
 from suds.client import Client
 
 #create the client endpoint
 client = Client('http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL')
-#print client
-
-#request the current weather of Chicago, IL
-chicago = client.service.GetCityWeatherByZIP(60637)
-#print chicago
 
 #request the weather information
 weathers = client.service.GetWeatherInformation()
 
-#weather_information = [elem for elem in weathers[0] if elem['WeatherID'] == chicago['WeatherID']][0]
-
-#print 'The weather in Chicago, IL is currently: %s' % weather_information['Description']
-
-
 def get_city_weather(zip_code):
     city = client.service.GetCityWeatherByZIP(zip_code)
+    if not city['Success']:
+    	return city['ResponseText'], None
     weather_information = [elem for elem in weathers[0] if elem['WeatherID'] == city['WeatherID']][0]
-    return 'The weather in %s, %s is currently: %s' % (city['City'], city['State'], weather_information['Description']), \
+    return u'The weather in %s, %s is currently: %s with %s°F' % (city['City'], city['State'], weather_information['Description'], city['Temperature']), \
            weather_information['PictureURL']
-
-
-def get_chicago_weather():
-    return get_city_weather(60637)
