f845a3dd943791534dd57329fd7d3d13ab927ffc
Gábor László Hajba <gabor.hajba@gmail.com>
2014-06-25 14:29:15 +0200

    changed to Jinja2
    added the template engine Jinja2 to the application

--- GAE/helloworld/helloworld.py
+++ GAE/helloworld/helloworld.py
@@ -2,8 +2,14 @@
 import webapp2
 import os
 import sys
+import jinja2
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 import weather_endpoint
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 MAIN_PAGE_HTML_HEAD = """\
 <html>
@@ -28,30 +34,23 @@ MAIN_PAGE_HTML_WEATHER_FORM = """\
       <div><input name="zip" type="number" size="10" min="10000" max="99999" required></div>
       <div><input type="submit" value="Ask for the weather"></div>
     </form>
-  </body>
-</html>
 """
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(MAIN_PAGE_HTML_HEAD)
-        self.response.write(u'Hello World in Chinese is: 你好世界')
-        self.response.write(MAIN_PAGE_HTML_WEATHER_FORM)
-        self.response.write(MAIN_PAGE_HTML_FOOT)
-
-class Weather(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(MAIN_PAGE_HTML_HEAD)
+      weather_description = None
+      weather_picture = None
+      if self.request.get('zip'):
         weather_description, weather_picture = weather_endpoint.get_city_weather(self.request.get('zip'))
-        if weather_picture is not None:
-            self.response.write('<img src="'+weather_picture+'" /> ')
-        self.response.write(weather_description)
-        self.response.write(MAIN_PAGE_HTML_WEATHER_FORM)
-        self.response.write(MAIN_PAGE_HTML_FOOT)
+      template_values = {
+        'weather_description': weather_description,
+        'weather_picture': weather_picture
+      }
+      self.response.headers['Content-Type'] = 'text/html'
+      template = JINJA_ENVIRONMENT.get_template('index.html')
+      self.response.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/weather', Weather)
+    ('/weather', MainPage)
 ], debug=True)
